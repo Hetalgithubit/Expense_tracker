9fad70c22b632703f03971f87cfa7df191b0d85f
Hetal KOshiya <koshiyahatel@gmail.com>
2026-09-01 12:07:28 +0530

    hetal2 theme border color red

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -7,7 +7,7 @@ class AppColors {
 
   static const card2 = Color(0xFF002612);
 
-  static const border = Color(0xFF006B35);
+  static const border = Color(0xF2E1335F);
 
   static const green = Color(0xFF16D365);
 
